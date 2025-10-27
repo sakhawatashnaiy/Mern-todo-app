b52b8e653e4f859ed816abb1ea373800d57ab999
sakhawatashnaiy <sakhawatashnaiy09@gmail.com>
2025-10-27 10:07:22 +0500

    new update

--- server.js
+++ server.js
@@ -7,8 +7,12 @@
 
  const app = express();
  app.use(cors({
-   origin: "http://localhost:5173",
+   origin: [
+    "http://localhost:5173",
+       "https://boisterous-pudding-12e56c.netlify.app/",
+      ],
    methods: ["GET", "POST", "PUT", "DELETE"],
+   credentials : true
  }));
  app.use(express.json());
 
@@ -25,69 +29,3 @@
  const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 
-
-
-//     //  comented code
-//    const express = require('express');
-// const fs = require('fs');
-// const cors = require('cors');
-// const app = express();
-
-// app.use(cors());
-// app.use(express.json());
-
-// const FILE_PATH = './data/tasks.json';
-
-// // Helper to read file
-// const readTasks = () => {
-//   const data = fs.readFileSync(FILE_PATH);
-//   return JSON.parse(data);
-// };
-
-// Helper to write file
-// const writeTasks = (tasks) => {
-//   fs.writeFileSync(FILE_PATH, JSON.stringify(tasks, null, 2));
-// };
-
-// // GET all tasks
-// app.get('/api/tasks', (req, res) => {
-//   const tasks = readTasks();
-//   res.json(tasks);
-// });
-
-// // POST add task
-// app.post('/api/tasks', (req, res) => {
-//   const tasks = readTasks();
-//   const newTask = {
-//     id: Date.now(),
-//     title: req.body.title,
-//     completed: false,
-//   };
-//   tasks.push(newTask);
-//   writeTasks(tasks);
-//   res.status(201).json(newTask);
-// });
-
-// // PATCH toggle complete
-// app.patch('/api/tasks/:id', (req, res) => {
-//   let tasks = readTasks();
-//   tasks = tasks.map((task) =>
-//     task.id === Number(req.params.id)
-//       ? { ...task, completed: !task.completed }
-//       : task
-//   );
-//   writeTasks(tasks);
-//   res.json({ message: 'Updated' });
-// });
-
-// // DELETE
-// app.delete('/api/tasks/:id', (req, res) => {
-//   const tasks = readTasks().filter(
-//     (task) => task.id !== Number(req.params.id)
-//   );
-//   writeTasks(tasks);
-//   res.json({ message: 'Deleted' });
-// });
-
-// const PORT = process.env.PORT || 10000;
-// app.listen(PORT, () => console.log(`Server running on ${PORT}`));
